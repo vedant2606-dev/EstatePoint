351a492739e9f11b04c0a9fc3be4180eec5153c3
vedant2606-dev <vedantpatil0306@gmail.com>
2025-08-31 23:05:40 +0530

    updated header and made the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import SignUp from './pages/SignUp'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -13,7 +14,9 @@ const App = () => {
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>}/>
+      </Route>
       <Route path='/about' element={<About/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
