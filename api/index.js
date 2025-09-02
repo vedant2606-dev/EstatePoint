0eb8a1f6dd04c0ca42289573bbf61ea3da599f20
vedant2606-dev <vedantpatil0306@gmail.com>
2025-09-02 23:02:26 +0530

    created update API

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
@@ -18,14 +19,12 @@ connectDB().then(() => {
   });
 });
 
-
-
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'INternal Server Error';
+  const message = err.message || "INternal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
-})
+});
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-
-router.get("/test", (req, res)=>{
-    res.send("Hello")
-})
+router.get("/test", (req, res) => {
+  res.send("Hello");
+});
+router.post("/update/:id",verifyToken, updateUser);
 
 export default router;
