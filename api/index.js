d67dc0b04f3ef5655aaac501e5e45084154c9c8e
vedant2606-dev <vedantpatil0306@gmail.com>
2025-09-03 11:29:10 +0530

    Added create listing API routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 connectDB().then(() => {
   app.listen(3000, () => {
