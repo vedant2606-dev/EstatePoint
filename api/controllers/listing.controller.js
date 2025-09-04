c5d868af983ebffdbb82f58f5889d87eaf0a66fb
vedant2606-dev <vedantpatil0306@gmail.com>
2025-09-04 10:48:29 +0530

    create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,7 +10,7 @@ export const createListing = async (req, res, next) => {
   }
 };
 
-export const deleteListing = async (req, res) => {
+export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
@@ -25,5 +25,25 @@ export const deleteListing = async (req, res) => {
     return res.status(201).json("Listing has been Deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings !"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    nexet(error);
   }
 };
